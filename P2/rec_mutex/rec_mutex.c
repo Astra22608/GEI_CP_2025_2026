67ab1d9c1ee52d8a636b80a472276cac395dab4b
Candela Naya López <candelacnl@gmail.com>
2026-03-16 20:11:51 +0100

    Eliminamos owner

--- P2/rec_mutex/rec_mutex.c
+++ P2/rec_mutex/rec_mutex.c
@@ -3,7 +3,6 @@
 
 int rec_mutex_init(rec_mutex_t *m) {
     m -> count = 0; //mutex desbloqueado
-    m -> owner = 0; //propietario del mutex en cada momento (sin dueño)
     return pthread_mutex_init(&m -> mutex, NULL);
 }
 
@@ -13,7 +12,7 @@ int rec_mutex_destroy(rec_mutex_t *m) {
 
 int rec_mutex_lock(rec_mutex_t *m) {
     pthread_t self = pthread_self(); //identificador del hilo
-    if (m -> count > 0 && pthread_equal(m -> owner, self)) { //comprobamos si el mismo hilo ya tiene mutex
+    if (m -> count > 0 && pthread_equal(m, self)) { //comprobamos si el mismo hilo ya tiene mutex
         m -> count++;
         return 0;
     }
@@ -21,7 +20,6 @@ int rec_mutex_lock(rec_mutex_t *m) {
     if (pthread_mutex_lock(&m -> mutex) != 0) { //si otro hilo lo tiene, esperamos
         return -1;
     }
-    m -> owner = self; //guardamos nuevo dueño
     m -> count = 1;
     return 0;
 };
@@ -33,7 +31,6 @@ int rec_mutex_unlock(rec_mutex_t *m) {
     }
     m -> count--;
     if (m -> count == 0) {
-        m -> owner = 0;
         pthread_mutex_unlock(&m -> mutex);
     }
     return 0;
@@ -42,14 +39,13 @@ int rec_mutex_unlock(rec_mutex_t *m) {
 
 int rec_mutex_trylock(rec_mutex_t *m) {
     pthread_t self = pthread_self ();
-    if (m -> count > 0 && pthread_equal(m -> owner, self)) {
+    if (m -> count > 0 && pthread_equal(m, self)) {
         m -> count++;
         return 0;
     }
     if (pthread_mutex_trylock(&m -> mutex) != 0) {
         return -1;
     }
-    m -> owner = self;
     m -> count = 1;
     return 0;
 }
