8e469a3968051847828786475c5d6439c80e9ca0
Candela Naya López <candelacnl@gmail.com>
2026-03-16 20:14:28 +0100

    Modificaciones en el programa de prueba

--- P2/rec_mutex/programa_prueba.c
+++ P2/rec_mutex/programa_prueba.c
@@ -12,7 +12,7 @@
 
 typedef struct {
     int array[ARRAY_SIZE];
-    rec_mutex_t mutexes[ARRAY_SIZE];     
+    rec_mutex_t mutexes[ARRAY_SIZE];
 } shared_data_t;
 
 typedef struct {
@@ -35,23 +35,14 @@ void* thread_func(void* arg) {
         int i = rand() % ARRAY_SIZE;
         int j = rand() % ARRAY_SIZE;
 
-        // Bloqueamos siempre en orden ascendente (evita deadlock)
-        if (i > j) {
-            int tmp = i; i = j; j = tmp;
-        }
-
-        rec_mutex_lock(shared->mutexes + i);
-        if (i != j) {                           // evita doble lock innecesario
-            rec_mutex_lock(shared->mutexes + j);
-        }
+        rec_mutex_lock(&shared->mutexes[i]);
+        rec_mutex_lock(&shared->mutexes[j]);
 
         printf("Hilo %d intercambiando %d -> %d\n", id, i, j);
         swap_positions(shared->array, i, j);
 
-        if (i != j) {
-            rec_mutex_unlock(shared->mutexes + j);
-        }
-        rec_mutex_unlock(shared->mutexes + i);
+        rec_mutex_unlock(&shared->mutexes[j]);
+        rec_mutex_unlock(&shared->mutexes[i]);
 
         usleep(SEGUNDOS);
     }
@@ -60,41 +51,36 @@ void* thread_func(void* arg) {
 }
 
 int main() {
-    srand(time(NULL));   
+    srand(time(NULL));
 
     pthread_t threads[NUM_THREADS];
     thread_arg_t thread_args[NUM_THREADS];
 
-    shared_data_t shared = {0};   // inicializa todo (incluyendo mutexes)
+    shared_data_t shared = {0};
 
-    // Inicializamos array y mutexes
     for (int i = 0; i < ARRAY_SIZE; i++) {
         shared.array[i] = i;
-        rec_mutex_init(shared.mutexes + i);
+        rec_mutex_init(&shared.mutexes[i]);
     }
-    
-    // Creamos hilos
+
     for (int i = 0; i < NUM_THREADS; i++) {
         thread_args[i].shared = &shared;
         thread_args[i].thread_id = i + 1;
         pthread_create(&threads[i], NULL, thread_func, &thread_args[i]);
     }
 
-    // Esperamos
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    // Resultado
     printf("\nArray final: ");
     for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", shared.array[i]);
     }
     printf("\n");
 
-    // Limpieza
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        rec_mutex_destroy(shared.mutexes + i);
+        rec_mutex_destroy(&shared.mutexes[i]);
     }
 
     return 0;
