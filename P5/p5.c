d2e3f0fb74bbe36a3e509f05ac8e8b4060c347b1
Candela Naya López <candelacnl@gmail.com>
2026-05-03 11:03:04 +0200

    Cambios tras defensa

--- P5/p5.c
+++ P5/p5.c
@@ -1,92 +1,126 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
+#include <stdlib.h>
+#include <string.h>
 
 void inicializaCadena(char *cadena, int n){
-    int i;
-    for(i=0; i<n/2; i++) cadena[i] = 'A';
-    for(i=n/2; i<3*n/4; i++) cadena[i] = 'C';
-    for(i=3*n/4; i<9*n/10; i++) cadena[i] = 'G';
-    for(i=9*n/10; i<n; i++) cadena[i] = 'T';
+  int i;
+  for(i=0; i<n/2; i++){
+    cadena[i] = 'A';
+  }
+  for(i=n/2; i<3*n/4; i++){
+    cadena[i] = 'C';
+  }
+  for(i=3*n/4; i<9*n/10; i++){
+    cadena[i] = 'G';
+  }
+  for(i=9*n/10; i<n; i++){
+    cadena[i] = 'T';
+  }
 }
 
-int MPI_BinomialBcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
-    int rank, numprocs;
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &numprocs);
-    int i = 1;
-    while ((1 << (i - 1)) < numprocs) {
-        int powers = (1 << (i - 1));
-        if (rank < powers) {
-            int target = rank + powers;
-            if (target < numprocs) {
-                MPI_Send(buffer, count, datatype, target, 0, comm);
-            }
-        } else if (rank < (1 << i)) {
-            int source = rank - powers;
-            MPI_Recv(buffer, count, datatype, source, 0, comm, MPI_STATUS_IGNORE);
-        }
-        i++;
+int MPI_FlattreeColectiva(void *buf,void *recvbuff, int count,MPI_Datatype datatype,MPI_Op op,int root,MPI_Comm comm){
+  int rank,numprocs,i;
+  MPI_Comm_size(comm,&numprocs);
+  MPI_Comm_rank(comm,&rank);
+  if(datatype != MPI_INT || op != MPI_SUM){
+    return -1;
+  }
+  if(rank==root){
+    int *recv = (int *)recvbuff;
+    int *send = (int *)buf;
+    for(i = 0; i < count; i++){
+      recv[i] = send[i];
     }
-    return MPI_SUCCESS;
+    int *temp = malloc(count * sizeof(int));
+    for(i=0;i<numprocs;i++){
+      if(i!=root){
+        MPI_Recv(temp,count,datatype,i,0,comm,MPI_STATUS_IGNORE);
+        for(int j=0;j<count;j++){
+          recv[j]+=temp[j];
+        }
+      }
+    }
+    free(temp);
+  }
+  else{
+    MPI_Send(buf,count,datatype,root,0,comm);
+  }
+  return MPI_SUCCESS;
 }
 
-int MPI_FlattreeColectiva(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm) {
-    int rank, numprocs;
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &numprocs);
-    if (rank == root) {
-        int total = *(int*)sendbuf;
-        for (int i = 0; i < numprocs; i++) {
-            if (i != root) {
-                int temp;
-                MPI_Recv(&temp, count, datatype, i, 0, comm, MPI_STATUS_IGNORE);
-                total += temp;
-            }
-        }
-        *(int*)recvbuf = total;
-    } else {
-        MPI_Send(sendbuf, count, datatype, root, 0, comm);
-    }
-    return MPI_SUCCESS;
-}
+int MPI_BinomialColectiva(void *buf, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
+    int rank, numprocs, res;
 
-int main (int argc, char *argv[]){
-    MPI_Init(&argc, &argv);
-    int numprocesos, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocesos);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int n, i, local_count = 0, total_count = 0;
-    char L, *cadena = NULL;
+    if (buf == NULL && count > 0) return MPI_ERR_ARG; 
+    if (count < 0) return MPI_ERR_ARG;
+    if (root != 0) return MPI_ERR_ROOT; // El algoritmo actual solo soporta root 0
 
-    if (rank == 0){ 
-        if(argc != 3){
-            printf("Uso: %s n L\n", argv[0]);
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
-        n = atoi(argv[1]);
-        L = *argv[2];
-    }
-
-    MPI_BinomialBcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_BinomialBcast(&L, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    cadena = (char *) malloc(n * sizeof(char));
-    if (cadena == NULL) MPI_Abort(MPI_COMM_WORLD, 1);
-
-    inicializaCadena(cadena, n);
-
-    for(i = rank; i < n; i += numprocesos){
-        if(cadena[i] == L) local_count++;
-    }
+    res = MPI_Comm_size(comm, &numprocs);
+    if (res != MPI_SUCCESS) return res;
     
-    MPI_FlattreeColectiva(&local_count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    res = MPI_Comm_rank(comm, &rank);
+    if (res != MPI_SUCCESS) return res;
 
-    if (rank == 0) {
-        printf("El numero de apariciones de la letra %c es %d\n", L, total_count);
+    int paso = 1;
+    while(paso < numprocs){
+        if(rank < paso){
+            int dest = rank + paso;
+            if(dest < numprocs){
+                res = MPI_Send(buf, count, datatype, dest, 0, comm);
+                if (res != MPI_SUCCESS) return res;
+            }
+        } else if(rank < 2 * paso){
+            int source = rank - paso;
+            res = MPI_Recv(buf, count, datatype, source, 0, comm, MPI_STATUS_IGNORE);
+            if (res != MPI_SUCCESS) return res;
+        }
+        paso *= 2;
     }
 
-    free(cadena);
-    MPI_Finalize();
-    return 0;
+    return MPI_SUCCESS;
 }
+
+int main(int argc,char *argv[]){
+    int numprocs,rank,n,i;
+    char L;
+    char *cadena;
+
+    MPI_Init(&argc,&argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    if(rank==0){
+      n = atoi(argv[1]);
+      L = *argv[2];
+    }
+
+    if(rank==0){//solo proceso 0 inicializa la cadena
+      
+      cadena = (char *) malloc(n*sizeof(char));
+      inicializaCadena(cadena, n);
+    }
+
+    MPI_BinomialColectiva(&n,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_BinomialColectiva(&L,1,MPI_CHAR,0,MPI_COMM_WORLD);
+    if(rank != 0){
+      cadena = (char *)malloc(n*sizeof(char));
+    }
+    MPI_BinomialColectiva(cadena, n, MPI_CHAR, 0, MPI_COMM_WORLD);
+    
+    int count = 0;
+    for(i=rank; i<n; i+=numprocs){
+    if(cadena[i] == L){
+        count++;
+      }
+    }
+    int total = 0;
+    MPI_FlattreeColectiva(&count, &total,1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
+    if(rank == 0){
+        printf("El numero de apariciones de la letra %c es %d\n", L, total);
+    }
+    free(cadena);
+
+    MPI_Finalize();
+  }
